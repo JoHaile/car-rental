67893683da74022089a5cc4675b59f84e3f2071c
John <yohannes.h93@gmail.com>
2025-09-10 03:55:37 +0300

    create the specific car carousel and make it responsive on different devices

--- app/(main)/vehicles/[carID]/page.tsx
+++ app/(main)/vehicles/[carID]/page.tsx
@@ -1,4 +1,12 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import prisma from "@/prisma";
+import Image from "next/image";
 import React from "react";
 
 interface Params {
@@ -16,13 +24,37 @@ async function page({ params }: Params) {
   });
 
   return (
-    <div>
-      car Id: {carID}
-      <ul>
-        <li>{car?.manufacture}</li>
-        <li>{car?.model}</li>
-        <li>{car?.features.enginePower}</li>
-      </ul>
+    <div className="my-8 w-full grid grid-cols-3 8 md:gap-0 justify-items-center">
+      <div className="col-span-3 md:col-span-2 text-center justify-items-center">
+        <Carousel
+          className="max-w-3/4 lg:max-w-5/6"
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+        >
+          <CarouselContent>
+            {car?.imageUrls.map((img) => (
+              <CarouselItem key={img}>
+                {
+                  <Image
+                    src={img ? img : "/car-image.jpg"}
+                    className="object-cover aspect-[16/9]"
+                    alt="image of a car"
+                    width={1000}
+                    height={1000}
+                  />
+                }
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+
+          <CarouselNext />
+          <CarouselPrevious />
+        </Carousel>
+      </div>
+
+      <div className="">{car?.description}</div>
     </div>
   );
 }
